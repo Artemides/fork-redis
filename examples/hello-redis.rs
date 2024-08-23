a7813df45dc40be15d15adb667bbefe663b6bb40
Artemides <edmpulasky@gmail.com>
2024-08-23 11:45:34 -0500

    shard shared db

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -3,7 +3,7 @@ use mini_redis::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut client = mini_redis::client::connect("127.0.0.1:4000").await?;
-    client.set("rust", "0.1.0".into()).await?;
+    // client.set("rust", "0.1.0".into()).await?;
     let value = client.get("rust").await?;
 
     println!("got value from the server; result={:?}", value);
